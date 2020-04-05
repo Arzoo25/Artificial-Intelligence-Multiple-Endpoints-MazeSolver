4d246d73dfd3338383bb9e5298de1c335d2ae89f
Arzoo253 <46155135+Arzoo253@users.noreply.github.com>
2020-04-05 21:06:48 +0100

    Dijkstra setup grid

---
+++ dijkstra.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  3 17:13:24 2020
+
+@author: Arzoo
+"""
+
+import turtle                    # import turtle library
+import sys
+from math import inf as infinity
+
+wn = turtle.Screen()               # define the turtle screen
+wn.bgcolor("black")                # set the background colour
+wn.title("A Djikstra Maze Solving Program")
+wn.setup(1300,700)                  # setup the dimensions of the working window
+
+
+# this is the class for the Maze
+class Maze(turtle.Turtle):               # define a Maze class
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")            # the turtle shape
+        self.color("white")             # colour of the turtle
+        self.penup()                    # lift up the pen so it do not leave a trail
+        self.speed(0)
+
+# this is the class for the finish line - green square in the maze
+class Green(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("green")
+        self.penup()
+        self.speed(0)
+
+class Blue(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("blue")
+        self.penup()
+        self.speed(0)
+
+
+# this is the class for the yellow or turtle
+class Red(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("red")
+        self.penup()
+        self.speed(0)
+
+class Yellow(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("yellow")
+        self.penup()
+        self.speed(0)
+
+
+# grid = [
+# "+++++++++++++++",
+# "+s+       + +e+",
+# "+ +++++ +++ + +",
+# "+ + +       + +",
+# "+ +   +++ + + +",
+# "+ + + +   + + +",
+# "+   + +   + + +",
+# "+++++ +   + + +",
+# "+     +   +   +",
+# "+++++++++++++++",
+# ]
+
+# grid = [
+# "+++++++++",
+# "+ ++s++++",
+# "+ ++ ++++",
+# "+ ++ ++++",
+# "+    ++++",
+# "++++ ++++",
+# "++++ ++++",
+# "+      e+",
+# "+++++++++",
+# ]
+
+# grid = [
+# "+++++++++++++++",
+# "+             +",
+# "+             +",
+# "+             +",
+# "+     e       +",
+# "+             +",
+# "+             +",
+# "+             +",
+# "+ s           +",
+# "+++++++++++++++",
+# ]
+grid = [
+"+++++++++++++++++++++++++++++++++++++++++++++++++++",
+"+               +                                 +",
+"+  ++++++++++  +++++++++++++  +++++++  ++++++++++++",
+"+s          +                 +               ++  +",
+"+  +++++++  +++++++++++++  +++++++++++++++++++++  +",
+"+  +     +  +           +  +                 +++  +",
+"+  +  +  +  +  +  ++++  +  +  +++++++++++++  +++  +",
+"+  +  +  +  +  +  +        +  +  +        +       +",
+"+  +  ++++  +  ++++++++++  +  +  ++++  +  +  ++   +",
+"+  +     +  +          +   +           +  +  ++  ++",
+"+  ++++  +  +++++++ ++++++++  +++++++++++++  ++  ++",
+"+     +  +     +              +              ++   +",
+"++++  +  ++++++++++ +++++++++++  ++++++++++  +++  +",
+"+  +  +                    +     +     +  +  +++  +",
+"+  +  ++++  +++++++++++++  +  ++++  +  +  +  ++   +",
+"+  +  +     +     +     +  +  +     +     +  ++  ++",
+"+  +  +  +++++++  ++++  +  +  +  ++++++++++  ++  ++",
+"+                       +  +  +              ++  ++",
+"+ ++++++             +  +  +  +  +++        +++  ++",
+"+ ++++++ ++++++ +++++++++    ++ ++   ++++++++++  ++",
+"+ +    +    +++ +     +++++++++ ++  +++++++    + ++",
+"+ ++++ ++++ +++ + +++ +++    ++    ++    ++ ++ + ++",
+"+ ++++    +     + +++ +++ ++ ++++++++ ++ ++ ++   ++",
+"+      ++ +++++++e+++     ++          ++    +++++++",
+"+++++++++++++++++++++++++++++++++++++++++++++++++++",
+ ]
+
+
+def setup_maze(grid):                          # define a function called setup_maze
+    global start_x, start_y, end_x, end_y ,start, end, nodes    # set up global variables for start and end locations
+    
+    nodes = [[None for x in range(len(grid[0]))] for y in range(len(grid))]
+    for y in range(len(grid)):                 # read in the grid line by line
+        for x in range(len(grid[y])):          # read each cell in the line
+            character = grid[y][x]             # assign the varaible "character" the the x and y location od the grid
+            screen_x = -588 + (x * 24)         # move to the x location on the screen staring at -588
+            screen_y = 288 - (y * 24)          # move to the y location of the screen starting at 288
+
+            if character == "+":
+                maze.goto(screen_x, screen_y)         # move pen to the x and y locaion and
+                maze.stamp()                          # stamp a copy of the turtle on the screen
+                walls.append((screen_x, screen_y))    # add coordinate to walls list
+                nodes[y][x] = None
+                
+            if character == " " or character == "e":
+                path.append((screen_x, screen_y))     # add " " and e to path list
+                nodes[y][x] = Node(x, y)
+            
+            if character == "e":
+                green.color("purple")
+                green.goto(screen_x, screen_y)       # send green sprite to screen location
+                end_x, end_y = screen_x,screen_y     # assign end locations variables to end_x and end_y
+                green.stamp()
+                green.color("green")
+                nodes[y][x] = Node(x, y)
+                end = (x, y)
+
+            if character == "s":
+                start_x, start_y = screen_x, screen_y  # assign start locations variables to start_x and start_y
+                red.goto(screen_x, screen_y)
+                nodes[y][x] = Node(x, y)
+                start = (x, y)
+
+
